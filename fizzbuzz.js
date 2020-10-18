bd4c4017da78f8c3160b071bf0a2b217f1a432ab
lukeashtonjohnson <lukeashtonjohnson@gmail.com>
2020-10-18 14:34:21 +0100

    Add for loop
    if else statement inside for loop checks the condition, resulting in FizzBuzz, Fizz, Buzz or just the number

---
+++ fizzbuzz.js
@@ -0,0 +1,12 @@
+'use strict';
+for ( let index = 0; index <= 100; index++ ) {
+  if ( index % 15 === 0) {
+    console.log( "FizzBuzz" );
+  } else if ( index % 3 === 0 ) {
+    console.log( "Fizz" );
+  } else if ( index % 5 === 0 ) {
+    console.log( "Buzz" );
+  } else {
+    console.log( index );
+  }
+}
